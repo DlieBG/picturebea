e02ec8c244fdea2d378748f5b62ff3c89aa4ff78
Benedikt Schwering <mail@benedikt-schwering.de>
2021-11-19 21:32:55 +0100

    small improvements

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Picturebea Classification AI';
+  title = 'PicturebAI';
 }
